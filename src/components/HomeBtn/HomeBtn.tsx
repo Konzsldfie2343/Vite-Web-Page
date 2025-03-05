39f0eb7f5a5d571952b45757589de866d00f3352
Konzsldfie2343 <146638369+Konzsldfie2343@users.noreply.github.com>
2025-03-05 11:45:32 +0900

    IMPROVE: HomeBtn

--- src/components/HomeBtn/HomeBtn.tsx
+++ src/components/HomeBtn/HomeBtn.tsx
@@ -5,19 +5,20 @@ import { Link } from "react-scroll"
 
 const HomeBtn = (): JSX.Element => {
     return (
-        <motion.div className={styles.HomeBtn}>
-            <div className={styles.Hexagon} style={{ backgroundColor: 'white', transform: "scale(0.9)" }}>
-                <div className={styles.Hexagon} style={{ backgroundColor: 'gray', transform: "scale(0.9)" }}>
-                    <Link
-                        to="MainVisual"
-                        smooth={true}
-                        duration={1000}
-                    >
+        <Link to="MainVisual" smooth={true} duration={1000} className={styles.HomeBtnContainer}>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1, duration: 1 }}
+                className={styles.HomeBtn}
+            >
+                <div className={styles.Hexagon} style={{ backgroundColor: 'white', transform: "scale(0.9)" }}>
+                    <div className={styles.Hexagon} style={{ backgroundColor: 'gray', transform: "scale(0.9)" }}>
                         <span className={styles.label}>TOP</span>
-                    </Link>
+                    </div>
                 </div>
-            </div>
-        </motion.div>
+            </motion.div>
+        </Link>
     )
 }
 
